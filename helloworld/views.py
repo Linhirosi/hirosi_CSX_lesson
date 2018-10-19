74025087867db693688729fd3d504c23ad5ea241
Linhirosi <42993638+Linhirosi@users.noreply.github.com>
2018-10-19 11:47:01 +0800

    Form表單

--- helloworld/views.py
+++ helloworld/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from myDatabase.models import commentBox #匯入SQLite
 from myDatabase.models import msg
-from django.http import HttpResponseRedict
+
 
 def index(request):
 	menutexts=["首頁(偽)","推薦影片(偽)","最多觀看(偽)","發燒影片(偽)","尊榮會員(偽)"]
@@ -20,6 +20,6 @@ def index(request):
 def message(request):
 	talker = request.GET.get("name",False)
 	comment=request.GET.get("msg",False)
-	db = msg.objects.create("user"=talker,"message"=comment)
+	db = msg.objects.create(user=talker,message=comment)
 	msgs1  = db.objects.all()
 	return render(request,'',locals())
