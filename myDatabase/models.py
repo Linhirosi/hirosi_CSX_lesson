7eb983c0b026157ffd20644a7432e88c9727fef9
Linhirosi <42993638+Linhirosi@users.noreply.github.com>
2018-10-19 11:32:13 +0800

    form 表單

--- myDatabase/models.py
+++ myDatabase/models.py
@@ -9,7 +9,7 @@ class commentBox(models.Model): #create a SQL sheet
         return self.talker+""+self.comment
 
 class msg(models.Model):
-    user=models.CharField(max_length,blank=False)
+    user=models.CharField(max_length=20,blank=False)
     message = models.CharField(max_length=50,blank=True)
     def __str__(self):
         return self.talker+""+self.comment
